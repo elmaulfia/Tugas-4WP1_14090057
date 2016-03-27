8827b916477589486774442e355d5138a315d5b3
Elmaulfia <elmaulfia12@gmail.com>
2016-03-27 19:18:43 +0700

    Added files via upload
    Project Faktorial

---
+++ while.php
@@ -0,0 +1,32 @@
+<html> 
+<body>
+<title> While </title>
+   <h1> while </h1>
+   <?php
+   
+			$a=1;
+		
+			while($a<=1)
+			{
+				echo "<font size=</h3>1</font><h3>"; 
+	            $a++;
+			}
+			{
+				echo "<font size=</h3>22</font><h3>"; 
+	            $a++;
+			}
+			{
+				echo "<font size=</h3>333</font><h3>"; 
+	            $a++;
+			}
+			{
+				echo "<font size=</h3>4444</font><h3>"; 
+	            $a++;
+			}
+			{
+				echo "<font size=</h3>55555</font><h3>"; 
+	            $a++;
+			}
+	?> 
+<body> 
+</html>
